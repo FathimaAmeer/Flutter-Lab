3c06b48196ad6942d6a3af5f79efc6e1c81e798f
Fathima Ameer <fathimaameer1601@gmail.com>
2025-12-02 20:53:10 +0530

    Add files via upload

---
+++ prgm1.dart
@@ -0,0 +1,12 @@
+//1.	Write a Dart program that takes a String, int, double, and bool, and prints a formatted sentence using string interpolation.
+void main() {
+  // Variables of different types
+  String name = "Fathima";
+  int age = 22;
+  double height = 5.4;
+  bool isStudent = true;
+
+  // Using string interpolation
+  print("Hello, my name is $name. I am $age years old, my height is $height feet, and it is $isStudent that I am a student.");
+}
+
